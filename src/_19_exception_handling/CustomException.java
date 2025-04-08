cf8a37faf52a28c917267b8a9724aa8d9b0731c9
pranay <npranay9899@gmail.com>
2025-04-08 23:28:40 +0530

    update

--- src/_19_exception_handling/CustomException.java
+++
@@ -1,7 +0,0 @@
-package _19_exception_handling;
-
-class CustomException extends Exception {
-    public CustomException(String message) {
-        super(message);
-    }
-}
--- src/_19_exception_handling/CustomExceptionExample.java
+++ src/_19_exception_handling/CustomExceptionExample.java
@@ -1,4 +1,25 @@
 package _19_exception_handling;
 
-public class CustomExceptionExample {
+class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
+
+class CustomExceptionExample {
+    static void validateMarks(int marks) throws CustomException {
+        if (marks < 0 || marks > 100) {
+            throw new CustomException("Marks should be between 0 and 100.");
+        } else {
+            System.out.println("Marks are valid: " + marks);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            validateMarks(110);
+        } catch (CustomException e) {
+            System.out.println("Caught custom exception: " + e.getMessage());
+        }
+    }
 }
