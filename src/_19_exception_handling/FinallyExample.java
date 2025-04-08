df84fb509aa9abf2e043211a829ff31dd5ccc146
pranay <npranay9899@gmail.com>
2025-04-08 23:36:11 +0530

    update

--- src/_19_exception_handling/FinallyExample.java
+++ src/_19_exception_handling/FinallyExample.java
@@ -2,6 +2,12 @@ package _19_exception_handling;
 
 class FinallyExample {
     public static void main(String[] args) {
+        /**
+         *  The finally block follows a try block or a catch block.
+         *  A finally block of code always executes, irrespective of occurrence of an Exception.
+         *  Using a finally block allows you to run any cleanup-type statements that you want to execute,
+         *  no matter what happens in the protected code.
+         **/
         try {
             int[] numbers = {1, 2, 3};
             System.out.println(numbers[3]); // ArrayIndexOutOfBoundsException
